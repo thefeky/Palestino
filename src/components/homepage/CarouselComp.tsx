9db16d5010a7e0ef3bffa6c6baac33f152ee8f52
thefeky <162041853+thefeky@users.noreply.github.com>
2025-04-30 18:00:09 +0300

    added carousel images

--- src/components/homepage/CarouselComp.tsx
+++ src/components/homepage/CarouselComp.tsx
@@ -4,6 +4,11 @@ import {
   CarouselItem,
 } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
+import img1 from "/assets/carousel/1.jpg";
+import img2 from "/assets/carousel/2.jpg";
+import img3 from "/assets/carousel/3.jpg";
+import img4 from "/assets/carousel/4.jpg";
+import img5 from "/assets/carousel/5.jpg";
 
 function CarouselComp({
   width,
@@ -22,13 +27,21 @@ function CarouselComp({
     <>
       <Carousel plugins={plugins} className={width}>
         <CarouselContent className="h-70">
-          <CarouselItem className={`${md} ${lg} bg-black`}>...</CarouselItem>
-          <CarouselItem className={`${md} ${lg} bg-red-500`}>...</CarouselItem>
-          <CarouselItem className={`${md} ${lg} bg-blue-500`}>...</CarouselItem>
-          <CarouselItem className={`${md} ${lg} bg-orange-500`}>
-            ...
+          <CarouselItem className={`${md} ${lg}`}>
+            <img src={img1} />
           </CarouselItem>
-          <CarouselItem className={`${md} ${lg} bg-pink-500`}>...</CarouselItem>
+          <CarouselItem className={`${md} ${lg}`}>
+            <img src={img2} />
+          </CarouselItem>
+          <CarouselItem className={`${md} ${lg}`}>
+            <img src={img3} />
+          </CarouselItem>
+          <CarouselItem className={`${md} ${lg}`}>
+            <img src={img4} />
+          </CarouselItem>
+          <CarouselItem className={`${md} ${lg}`}>
+            <img src={img5} />
+          </CarouselItem>
         </CarouselContent>
       </Carousel>
     </>
